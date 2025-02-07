e5376371a4d407db04bf311138c0e05f4d8669f5
krotovcode <youngspirits@yandex.ru>
2025-02-07 23:51:12 +0300

    interface

--- FundMe.sol
+++ FundMe.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+interface AggregatorV3Interface {
+    
+}
+
 contract FundMe {
 
     uint256 public myValue = 1;
@@ -8,6 +12,15 @@ contract FundMe {
     function fund() public payable  {   
         myValue = myValue + 2;
         require(msg.value > 1e18, "not enough");
+    }
+
+    function getPrice() public {
+
+    }
+
+    function getConversionRate() public {
+        //0x694AA1769357215DE4FAC081bf1f309aDC325306
+
     }
 
 

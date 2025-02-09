db6c9f5440be91412f3f9e44e7c56655a01ff298
krotovcode <youngspirits@yandex.ru>
2025-02-09 23:59:31 +0300

    getVersion

--- FundMe.sol
+++ FundMe.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-interface AggregatorV3Interface {
-    
-}
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 contract FundMe {
 
@@ -20,7 +19,10 @@ contract FundMe {
 
     function getConversionRate() public {
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
+    }
 
+    function getVersion() public view returns (uint256) {
+        return    AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
 

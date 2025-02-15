d5c080a859d077d45154d5d95f7afc0dfe18fae5
krotovcode <youngspirits@yandex.ru>
2025-02-15 22:15:49 +0300

    Final - ready

--- FundMe.sol
+++ FundMe.sol
@@ -1,35 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+    uint256 public minUsd = 5e18;
 
-    uint256 public myValue = 1;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public adressToAmountFunded;
 
-    function fund() public payable  {   
-        myValue = myValue + 2;
-        require(msg.value > 1 ether, "not enough");
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minUsd, "not enough");
+        funders.push(msg.sender);
+        adressToAmountFunded[msg.sender] = adressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256 (price * 1e10);
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
     }
 
-    function getPrice2() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256 (price);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethAmount*ethPrice) / 1e18;
+        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
         return ethAmountInUsd;
     }
-
 }

7082182413baad81de734438397b04f887867db9
krotovcode <youngspirits@yandex.ru>
2025-02-15 23:13:14 +0300

    Library

--- FundMe.sol
+++ FundMe.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "PriceConverter.sol";
 
 contract FundMe {
+
+    using PriceConverter for uint256;
+
     uint256 public minUsd = 5e18;
 
     address[] public funders;
@@ -11,27 +14,9 @@ contract FundMe {
 
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minUsd, "not enough");
+        require(msg.value.getConversionRate() >= minUsd, "not enough");
         funders.push(msg.sender);
         adressToAmountFunded[msg.sender] = adressToAmountFunded[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
-        //0x694AA1769357215DE4FAC081bf1f309aDC325306
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
-        return ethAmountInUsd;
-    }
 }

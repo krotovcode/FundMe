6a2e7627657b4e7a02a7b6eea96975d7d600f72a
krotovcode <youngspirits@yandex.ru>
2025-02-27 21:02:54 +0300

    witdrawl

--- FundMe.sol
+++ FundMe.sol
@@ -16,7 +16,21 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate() >= minUsd, "not enough");
         funders.push(msg.sender);
-        adressToAmountFunded[msg.sender] = adressToAmountFunded[msg.sender] + msg.value;
+        adressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            adressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        payable(msg.sender).transfer(address(this).balance);
+        bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        require(sendSuccess, "not success");
+        (bool callSuccess,) = payable (msg.sender).call{value: address(this).balance}('');
+        require(callSuccess, "no transfer");
+
     }
 
 }

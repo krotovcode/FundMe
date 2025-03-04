4ab3e112231b337410fcaa28d8fe37b7026e14a5
krotovcode <youngspirits@yandex.ru>
2025-03-04 17:14:10 +0300

    fund - final

--- FundMe.sol
+++ FundMe.sol
@@ -12,6 +12,13 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public adressToAmountFunded;
 
+    address public owner;
+
+
+    constructor() {
+        owner = msg.sender;
+    }
+
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= minUsd, "not enough");
@@ -19,18 +26,24 @@ contract FundMe {
         adressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             adressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        payable(msg.sender).transfer(address(this).balance);
-        bool sendSuccess = payable (msg.sender).send(address(this).balance);
-        require(sendSuccess, "not success");
+        // payable(msg.sender).transfer(address(this).balance);
+        // bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        // require(sendSuccess, "not success");
         (bool callSuccess,) = payable (msg.sender).call{value: address(this).balance}('');
         require(callSuccess, "no transfer");
 
     }
 
+    modifier onlyOwner(){
+        require(msg.sender == owner, "only owner");
+        _;
+    } 
+    
 }
+
